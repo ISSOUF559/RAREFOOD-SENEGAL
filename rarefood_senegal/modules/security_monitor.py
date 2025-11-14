c34c3f1daa807e78b12cd6f0cdede2160d872324
ISSOUF559 <issoufimouhichimiwa@gmail.com>
2025-11-14 16:32:58 +0000

    Create security_monitor.py

---
+++ rarefood_senegal/modules/security_monitor.py
@@ -0,0 +1,10 @@
+# rarefood_senegal/modules/security_monitor.py
+from datetime import datetime
+
+logs = []
+
+def log_event(action):
+    logs.append({"timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "action": action})
+
+def get_logs():
+    return logs
