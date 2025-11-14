b694a3e15308c35ba14cd89e36047055e1865c00
ISSOUF559 <issoufimouhichimiwa@gmail.com>
2025-11-14 16:41:30 +0000

    Create testimonial_manager.py

---
+++ rarefood_senegal/modules/testimonial_manager.py
@@ -0,0 +1,8 @@
+# rarefood_senegal/modules/testimonial_manager.py
+temoins = [
+    {"nom": "Khadija", "region": "Fouta", "message": "Je suis fière de partager mes produits avec toute la région."},
+    {"nom": "Moussa", "region": "Casamance", "message": "La certification m’a permis d’atteindre de nouveaux clients."}
+]
+
+def get_all():
+    return temoins
